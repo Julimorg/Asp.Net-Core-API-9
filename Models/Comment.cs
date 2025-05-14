c3ffc7a8a5cf1c4f83a75be3de64c9a1068763a8
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-05-14 13:43:03 +0700

    config

---
+++ Models/Comment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        // Navigation Property
+        // -> Day la 1 Reference NP the hien mqh Many - 1
+        // -> Nhieu Comment se co duy nhat 1 Stocks
+        public int? StockId { get; set; }
+        public Stocks? Stock { get; set; }
+    }
+}
